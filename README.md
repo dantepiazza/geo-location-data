520bd15d72818dab2fbe574fcf51f53b07cf36d3
Dante Piazza Quiroga <dante.7h@gmail.com>
2024-04-01 17:30:21 -0300

    Cache location registration was added to speed up searches

--- README.md
+++ README.md
@@ -1,2 +1,93 @@
-# geo-location-data
-A JSON library to display countries, states, counties and cities with PHP
+# Geo locations data
+A JSON library to display countries, states, counties and cities with PHP. 
+
+NOTE: This package is under development and should not use ID values as final
+
+## Usage
+```php
+use Dantepiazza\GeoLocationData\Location;
+
+$location = new Location();
+
+// Regenerata a chache file
+$location -> cache();
+
+// Search locations by city name
+$location -> search('Buenos Ai');
+
+// Get location by city name
+$location -> get(6344);
+
+// Returns only the list of countries
+$location -> countries();
+
+// Get country by id
+$location -> country('id', 11);
+
+// Returns only the list of states
+$location -> states();
+
+// Get state by id
+$location -> state(788);
+
+// Returns only the list of counties in the country code
+$location -> counties('AR');
+
+// Get county by id
+$location -> county('AR', 216);
+
+// Returns only the list of cities in the country code
+$location -> cities('AR');
+
+// Get city by id
+$location -> city('AR', 4867);
+```
+
+## Responses
+
+##### Location data
+```php
+country_id: int
+country_name: string
+country_code: string
+state_id: int
+state_name: string
+county_id: int
+county_name: string
+city_id: int
+city_name: string
+zip_code: string
+name: string
+```
+
+##### Country data
+```php
+id: int
+code: string
+name: string
+```
+
+##### State data
+```php
+id: int
+country_id: int
+name: string
+```
+
+##### County data
+```php
+id: int
+country_id: int
+state_id: int
+name: string
+```
+
+##### City data
+```php
+id: int
+country_id: int
+state_id: int
+county_id: int
+zip_code: string
+name: string
+```
--- src/Location.php
+++ src/Location.php
@@ -3,68 +3,62 @@
 namespace Dantepiazza\GeoLocationData;
 
 class Location {
-    protected $_countries = [];
-    protected $_states = [];
-    protected $_counties = [];
-    protected $_cities = [];
     protected $_locations = [];
+    protected $_cache = __DIR__.'/data/cache.json';
 
     public function __construct(){
-        $this -> _countries = $this -> load_json_data('countries');
-        $this -> _states = $this -> load_json_data('states');
-
-        foreach($this -> _countries as $country){
-            $this -> _counties = array_merge($this -> _counties, $this -> load_json_data($country -> code.'/counties'));
-            $this -> _cities = array_merge($this -> _cities, $this -> load_json_data($country -> code.'/cities'));
+        if(!file_exists($this -> _cache)){
+            $this -> cache();
         }
 
-        foreach($this -> _cities as $city){
-            $this -> _locations[] = $this -> location($city);
-        }
+        $this -> _locations = $this -> load_json_data();
     }
 
-    private function location($city){
-        if(!$city) { 
-            return (object) [
-                'country_id' => 0,
-                'country_name' => '',
+    public function cache(){
+        $countries = $this -> load_json_data('countries');
+        $states = $this -> load_json_data('states');
+        $counties = [];
+        $cities = [];
+
+        foreach($countries as $country){
+            $counties = array_merge($counties, $this -> load_json_data($country -> code.'/counties'));
+            $cities = array_merge($cities, $this -> load_json_data($country -> code.'/cities'));
+        }
+
+        $this -> _locations = [];
+        
+        foreach($cities as $city){
+            $country = $this -> _get($countries, 'id', $city -> country_id);
+            $state = $this -> _get($states, 'id', $city -> state_id);
+            $county = $this -> _get($counties, 'id', $city -> county_id);
+            
+            $this -> _locations[$city -> id] =  (object) [
+                'country_id' => $country -> id,
+                'country_name' => $country -> name,
+                'country_code' => $country -> code,
     
-                'state_id' => 0,
-                'state_name' => '',
+                'state_id' => $state -> id,
+                'state_name' => $state -> name,
     
-                'county_id' => 0,
-                'county_name' => '',
+                'county_id' => $county -> id,
+                'county_name' => $county -> name,
     
-                'city_id' => 0,
-                'city_name' => '',
-                'zip_code' => '',
-                'name' => '',
+                'city_id' => $city -> id,
+                'city_name' => $city -> name,
+                'zip_code' => $city -> zip_code,
+                'name' => $city -> name.' ('.$city -> zip_code.'), '.$state -> name.', '.$country -> name,
             ];
         }
 
-        $country = $this -> country('id', $city -> country_id);
-        $state = $this -> state($city -> state_id);
-        $county = $this -> county($city -> county_id);
+        $fileData = json_encode($this -> _locations, JSON_PRETTY_PRINT);
         
-        return (object) [
-            'country_id' => $country -> id,
-            'country_name' => $country -> name,
-
-            'state_id' => $state -> id,
-            'state_name' => $state -> name,
-
-            'county_id' => $county -> id,
-            'county_name' => $county -> name,
-
-            'city_id' => $city -> id,
-            'city_name' => $city -> name,
-            'zip_code' => $city -> zip_code,
-            'name' => $city -> name.' ('.$city -> zip_code.'), '.$state -> name.', '.$country -> name,
-        ];
+        $save = fopen($this -> _cache, 'w');        
+        fwrite($save, $fileData);
+        fclose($save);
     }
 
     private function load_json_data($source = null) {
-        $file = __DIR__.'/data/'.strtolower($source).'.json';
+        $file = !is_null($source) ? __DIR__.'/data/'.strtolower($source).'.json' : $this -> _cache;
         
         if(file_exists($file)){
             return json_decode(file_get_contents($file));
@@ -90,42 +84,42 @@ class Location {
     }
 
     public function search(string $query){
-        return $this -> _search($this -> _locations, 'name', $query, false);
+        return $this -> _search((array) $this -> _locations, 'name', $query, false);
     }
 
     public function get(int $city){
-        return $this -> location($this -> city($city));
+        return $this -> _get((array) $this -> _locations, 'city_id', $city);
     }
 
     public function countries(){
-        return $this -> _countries;
+        return $this -> load_json_data('countries');
     }
     
     public function country(string $key, $value){
-        return $this -> _get($this -> _countries, $key, $value);
+        return $this -> _get((array) $this -> countries(), $key, $value);
     }
 
     public function states(){
-        return $this -> _states;
+        return $this -> load_json_data('states');
     }
     
     public function state(int $id){
-        return $this -> _get($this -> _states, 'id', $id);
+        return $this -> _get((array) $this -> states(), 'id', $id);
     }
 
-    public function counties(){
-        return $this -> _counties;
+    public function counties(string $code){
+        return $this -> load_json_data($code.'/counties');
     }
     
-    public function county(int $id){
-        return $this -> _get($this -> _counties, 'id', $id);
+    public function county(string $code, int $id){
+        return $this -> _get($this -> counties($code), 'id', $id);
     }
 
-    public function cities(){
-        return $this -> _cities;
+    public function cities(string $code){
+        return $this -> load_json_data($code.'/cities');
     }
     
-    public function city(int $id){
-        return $this -> _get($this -> _cities, 'id', $id);
+    public function city(string $code, int $id){
+        return $this -> _get((array) $this -> cities($code), 'id', $id);
     }
 }
